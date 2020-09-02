c92c18cda247d32434af17cf79e3d80fa0269c25
Chris Gradwohl <christophergradwohl@gmail.com>
2020-09-02 07:14:41 -0700

    adds website and handler.

--- handler.js
+++ handler.js
@@ -1,18 +1,17 @@
 'use strict';
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+const template = fs.readFileSync('static/index.html', 'utf-8');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.hello = async event => {
+  const html = Mustache.render(template, view);
+
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8'
+    },
+    body: html
+  }
+
+  return response;
 };
